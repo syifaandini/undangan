6c6234756d3ed89b9a239c40fd58bcc161c76765
syifaandini <syifandin@gmail.com>
2021-03-07 18:05:26 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import VueCountdownTimer from 'vuejs-countdown-timer';
 
-
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
 Vue.use(VueCountdownTimer);
