67983722d3a05d8055dc4702ca7206821569d6cf
syifaandini <syifandin@gmail.com>
2021-03-25 12:07:04 +0700

    tes masonry

--- src/components/modal/GalleryModal.vue
+++ src/components/modal/GalleryModal.vue
@@ -2,15 +2,78 @@
   <base-modal :idModal="'gallery'">
     <template>
       <h1 class="title-modal text-center mb-0">Gallery</h1>
-      <b-row>
+      <!-- <div class="row">
+        <div class="column">
+          <img
+            src="../../assets/photos/1.jpg"
+            style="width: 100%"
+            v-b-modal.image1
+          />
+          <img
+            src="../../assets/photos/3.jpg"
+            style="width: 100%"
+            v-b-modal.image3
+          />
+        </div>
+        <div class="column">
+          <img
+            src="../../assets/photos/2.jpg"
+            style="width: 100%"
+            v-b-modal.image2
+          />
+          <img
+            src="../../assets/photos/4.jpg"
+            style="width: 100%"
+            v-b-modal.image4
+          />
+        </div>
+        <div class="column">
+          <img
+            src="../../assets/photos/2.jpg"
+            style="width: 100%"
+            v-b-modal.image2
+          />
+          <img
+            src="../../assets/photos/4.jpg"
+            style="width: 100%"
+            v-b-modal.image4
+          />
+        </div>
+      </div> -->
+      <section id="photos">
+        <img src="../../assets/photos/1.jpg" alt="foto" v-b-modal.image1 />
+        <img src="../../assets/photos/2.jpg" alt="foto" v-b-modal.image2 />
+        <img src="../../assets/photos/3.jpg" alt="foto" v-b-modal.image3 />
+        <img src="../../assets/photos/4.jpg" alt="foto" v-b-modal.image4 />
+        <img src="../../assets/photos/2.jpg" alt="foto" v-b-modal.image2 />
+        <img src="../../assets/photos/3.jpg" alt="foto" v-b-modal.image3 />
+        <img src="../../assets/photos/4.jpg" alt="foto" v-b-modal.image4 />
+        <img src="../../assets/photos/2.jpg" alt="foto" v-b-modal.image2 />
+        <img src="../../assets/photos/1.jpg" alt="foto" v-b-modal.image1 />
+        <img src="../../assets/photos/4.jpg" alt="foto" v-b-modal.image4 />
+      </section>
+
+      <image-modal idModal="image1">
+        <img src="../../assets/photos/1.jpg" style="width: 100%" />
+      </image-modal>
+      <image-modal idModal="image2">
+        <img src="../../assets/photos/2.jpg" style="width: 100%" />
+      </image-modal>
+      <image-modal idModal="image3">
+        <img src="../../assets/photos/3.jpg" style="width: 100%" />
+      </image-modal>
+      <image-modal idModal="image4">
+        <img src="../../assets/photos/4.jpg" style="width: 100%" />
+      </image-modal>
+      <!-- <b-row>
         <b-col cols="12" md="6">
           <div class="qrcode-wrapper">
-            <img src="../../assets/qrcode/bca.png" alt="bca" class="qrcode" />
+            <img src="../../assets/photos/1.jpg" alt="foto" class="qrcode" />
           </div>
         </b-col>
         <b-col cols="12" md="6">
           <div class="qrcode-wrapper">
-            <img src="../../assets/qrcode/bca.png" alt="bca" class="qrcode" />
+            <img src="../../assets/photos/2.jpg" alt="bca" class="qrcode" />
           </div>
         </b-col>
       </b-row>
@@ -49,13 +112,14 @@
             <img src="../../assets/qrcode/bca.png" alt="bca" class="qrcode" />
           </div>
         </b-col>
-      </b-row>
+      </b-row> -->
     </template>
   </base-modal>
 </template>
 
 <script>
 import BaseModal from "../ui/BaseModal";
+import ImageModal from "./ImageModal.vue";
 // function copy() {
 //   var copyText = document.querySelector("#code1");
 //   copyText.select();
@@ -63,7 +127,7 @@ import BaseModal from "../ui/BaseModal";
 // }
 
 export default {
-  components: { BaseModal },
+  components: { BaseModal, ImageModal },
   data() {
     return {
       test: "123456",
@@ -86,24 +150,95 @@ export default {
 </script>
 
 <style scoped>
-.qrcode-wrapper {
-  margin-bottom: 10px;
-  padding-bottom: 10px;
-  background-color: white;
+#photos {
+  /* Prevent vertical gaps */
+  line-height: 0;
+
+  -webkit-column-count: 2;
+  -webkit-column-gap: 0px;
+  -moz-column-count: 2;
+  -moz-column-gap: 0px;
+  column-count: 2;
+  column-gap: 0px;
+}
+
+#photos img {
+  /* Just in case there are inline attributes */
+  width: 100% !important;
+  height: auto !important;
+}
+
+#photos img:focus{
+  outline: none
+}
+
+.row {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+  padding: 0 4px;
+}
+
+/* Create four equal columns that sits next to each other */
+.column {
+  -ms-flex: 50%; /* IE10 */
+  flex: 50%;
+  max-width: 50%;
+  padding: 0 4px;
+}
+
+.column img {
+  margin-top: 8px;
+  vertical-align: middle;
   width: 100%;
-  text-align: center;
-  border-radius: 15px;
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
-  cursor: pointer;
 }
-.qrcode-wrapper img {
-  margin: 20px 10px;
-  width: 80%;
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 800px) {
+  .column {
+    -ms-flex: 100%;
+    flex: 100%;
+    max-width: 50%;
+  }
 }
-.qrcode-wrapper p {
-  margin-bottom: 0;
-  line-height: 26px;
-  /* font-size: 1.5rem; */
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    -ms-flex: 100%;
+    flex: 100%;
+    max-width: 100%;
+  }
+}
+
+@media (max-width: 1200px) {
+  #photos {
+    -moz-column-count: 4;
+    -webkit-column-count: 4;
+    column-count: 4;
+  }
+}
+@media (max-width: 1000px) {
+  #photos {
+    -moz-column-count: 3;
+    -webkit-column-count: 3;
+    column-count: 3;
+  }
+}
+@media (max-width: 800px) {
+  #photos {
+    -moz-column-count: 2;
+    -webkit-column-count: 2;
+    column-count: 2;
+  }
+}
+@media (max-width: 400px) {
+  #photos {
+    -moz-column-count: 1;
+    -webkit-column-count: 1;
+    column-count: 1;
+  }
 }
 </style>
 
---
+++ src/components/modal/ImageModal.vue
@@ -0,0 +1,99 @@
+<template>
+  <b-modal
+    :id="idModal"
+    hide-footer
+    centered
+    backdrop
+    header-class="imagemodal__header"
+    body-class="imagemodal__body"
+    content-class="imagemodal__content"
+    modal-class="customscroll__imagemodal"
+  >
+    <template #modal-header="{ close }">
+      <img
+        src="../../assets/close.svg"
+        class="close__imagemodal"
+        alt="close"
+        @click="close()"
+      />
+    </template>
+    <div class="block">
+      <slot></slot>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: ["idModal"],
+};
+</script>
+
+<style>
+.dialog-title {
+  color: #365da5;
+  font-size: 22px;
+  letter-spacing: 1.2px;
+  font-weight: bold;
+}
+.imagemodal__header {
+  padding: 0;
+  border-bottom: 0;
+}
+.close__imagemodal {
+  overflow: hidden;
+  margin-left: auto;
+  position: absolute;
+  z-index: 10000;
+  right: 3%;
+  top: 4%;
+  width: 3%;
+}
+.close__imagemodal:hover {
+  cursor: pointer;
+}
+.imagemodal__body {
+  padding: 0;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  max-height: 90vh;
+}
+.modal-dialog-scrollable .modal-content.imagemodal__content {
+  overflow: hidden;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 15px;
+  width: fit-content;
+  height: fit-content;
+  max-height: 95vh;
+}
+.btn {
+  font-size: 1.5rem;
+}
+
+
+/* 20:9 */
+@media only screen and (min-aspect-ratio: 9/20) and (orientation: portrait) {
+  .modal-dialog-scrollable .modal-content.imagemodal__content {
+    width: 94vmin;
+  }
+  .close__imagemodal {
+    top: 2%;
+    right: 4%;
+    width: 5%;
+  }
+}
+
+/* Portrait Ipad */
+@media only screen and (min-width: 768px) and (max-height: 1366px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 1.5) {
+  .modal-dialog-scrollable .modal-content.imagemodal__content {
+    width: 35vh;
+    max-width: 40vh;
+    max-height: 50vh;
+  }
+  .close__imagemodal {
+    top: 2%;
+    right: 2%;
+    width: 3%;
+  }
+}
+</style>
